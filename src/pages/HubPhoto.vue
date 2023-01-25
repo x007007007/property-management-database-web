086cd7161a0ef89658d45c48b0866ddbde83ba82
xxc <x007007007@hotmail.com>
2023-01-26 00:29:10 +0800

    add more information on photo page

--- src/pages/HubPhoto.vue
+++ src/pages/HubPhoto.vue
@@ -1,11 +1,24 @@
 <template>
   <q-page class="row items-center justify-evenly">
+    <q-card v-if="currentHub && !photo">
+      <q-card-section>
+        {{ currentHub.code }} : {{currentHub.name}}
+      </q-card-section>
+      <q-card-section  v-if="currentHub.location_detail">
+        {{ currentHub.location_detail.path }}
+      </q-card-section>
+      <q-img v-if="currentHub.image"
+        :src="currentHub.image"
+      >
+      </q-img>
+    </q-card>
     <input ref="photoInputRef" class="image-input" type="file" accept="image/*" capture="camera" @change="onTakePhoto"/>
     <div v-if="photo" class="col-11">
       <q-btn-group push v-show="!isUploading">
         <q-btn @click="takePhoto">重拍</q-btn>
         <q-btn @click="submit">提交</q-btn>
       </q-btn-group>
+      <label>        {{ currentHub.location_detail.path }}: {{currentHub.name}}</label>
       <q-img
         fit="contain"
         :src="photo"></q-img>
@@ -16,7 +29,6 @@
   </q-page>
 </template>
 
-
 <script>
 import {defineComponent} from 'vue'
 
@@ -26,11 +38,11 @@ export default defineComponent({
 </script>
 
 <script setup>
-import {ref} from 'vue'
+import { ref, onBeforeMount } from 'vue'
 import {useRoute} from 'vue-router'
 import axios from 'axios'
 
-
+const currentHub = ref()
 const photo = ref()
 const photoInputRef = ref()
 const route = useRoute()
@@ -73,6 +85,16 @@ const submit = async (e) => {
   }
 }
 
+
+const getHubDetail = async () => {
+  const code = route.params.code
+  return (await axios.get(`/api/v1/hub/code/${code}/`)).data
+}
+
+
+onBeforeMount(async () => {
+    currentHub.value = await getHubDetail()
+})
 </script>
 
 <style lang="scss">
